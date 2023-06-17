72da5ba0c8045bffb20b6824a7addc763504aa7d
younesbenfaida2020@gmail.com <younesbenfaida2020@gmail.com>
2023-06-17 20:09:10 +0100

    first commit

---
+++ todolist/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using todolist.Models;
+
+namespace todolist.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly TodolistContext _dbcontext;
+
+        public UserController(TodolistContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+
+
+
+   
+
+
+
+
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _dbcontext.Users.FirstOrDefaultAsync(x => x.Email == userObj.Email && x.Password == userObj.Password);
+
+            if (user == null)
+                return NotFound(new { Message = "User not found" });
+
+            return Ok(new
+            {
+                Message = "Login Success"
+            });
+        }
+
+
+
+
+        [HttpPost("register")]
+        public async   Task<IActionResult> RegisterUser([FromBody] User userObj) {
+
+
+
+            userObj.Iduser = Guid.NewGuid();
+            if(userObj==null)
+                return BadRequest();
+
+            await _dbcontext.Users.AddAsync(userObj);
+            await _dbcontext.SaveChangesAsync();
+
+            return Ok(
+                 new
+                 {
+                     Message = "wa hahowa tzad "
+                 }
+                ) ;
+
+
+
+        }
+    }
+
+
+}
---
+++ todolist/Program.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using todolist.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//cors 
+
+builder.Services.AddCors( option=>
+{
+    option.AddPolicy("MyPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+        });
+}
+    
+    );
+
+
+builder.Services.AddDbContext<TodolistContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+/*
+
+builder.Services.AddScoped<UserRepository>();*/
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+
+var app = builder.Build();
+
+//cors
+
+app.UseCors("MyPolicy");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+
+
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+
